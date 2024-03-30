5a0eca038f08494bf3d4059b54bd6e23293645ec
Tim Cera <tim@cerazone.net>
2024-03-30 02:24:45 -0400

    build: change setup.py to publish.py

---
+++ publish.py
@@ -0,0 +1,26 @@
+"""Publish package to PyPI."""
+
+import shlex
+import subprocess
+
+PKG_NAME = "tsgettoolbox"
+
+with open("VERSION", encoding="ascii") as version_file:
+    version = version_file.readline().strip()
+
+subprocess.run(shlex.split("pyclean ."), check=True)
+
+subprocess.run(shlex.split("python3 -m build --sdist"), check=True)
+subprocess.run(shlex.split("python3 -m build --wheel"), check=True)
+sdist = f"dist/{PKG_NAME}-{version}.tar.gz"
+wheel = f"dist/{PKG_NAME}-{version}*.whl"
+
+for file in [sdist, wheel]:
+    subprocess.run(
+        shlex.split(f"twine check {file}"),
+        check=True,
+    )
+    subprocess.run(
+        shlex.split(f"twine upload --skip-existing {file}"),
+        check=True,
+    )
